ecf8ca47df680052619b8ab767157d4bb5c8f605
Dan Electronics <SimpleElectronicsYT@gmail.com>
2026-01-23 18:08:50 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+#Importing from other modules
+from models import Character
+
+#Make an instance of the character class - name, hp, stength
+character_one = Character("Hero", 100, 10)
+print(character_one)
---
+++ models.py
@@ -0,0 +1,9 @@
+class Character():
+    def __init__(self, name, hp, strength):
+        self.name = name
+        self.hp = hp
+        self.strength = strength
+        
+    def __str__(self):
+        return f"{self.name} (HP: {self.hp})"
+        
